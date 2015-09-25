85149b5e9456a3af991de09cd995ab16886caf90
sydorenkovd <sydorenkovd@gmail.com>
2015-09-25 15:57:24 +0300

    add XSL/T document, and XSL procesor helped by PHP

--- xml/xslt.php
+++ xml/xslt.php
@@ -1,15 +1,16 @@
 <?php
 // Создание объекта XML
-	
+	$xml = new DomDocument();
 // Загрузка XML документа
-	
+	$xml->load("catalog.xml");
 // Создание объекта XSL
-	
+	$xsl = new DomDocument();
 // Загрузка XSL документа
-	
+	$xsl->load("catalog.xsl");
 // Создание XSLT парсера
-	
+	$processor = new XSLTProcessor();
 // Загрузка XSL объекта
-	
+	$processor->importStylesheet($xsl);
 // Преобразование
+	echo $processor->transformToXML($xml);
 ?>
