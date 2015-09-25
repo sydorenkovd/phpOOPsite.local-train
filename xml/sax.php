db905c250dede1c77acb8be1d7797c28eb13389b
sydorenkovd <sydorenkovd@gmail.com>
2015-09-25 10:23:44 +0300

    create table with XML-file and SAX(XMLpath)

--- xml/sax.php
+++ xml/sax.php
@@ -1,11 +1,29 @@
 <?php 
   header( "Content-Type: text/html;charset=utf-8"); 
   // Создание парсера 
+  $sax = xml_parser_create("utf-8");
   // Функция обработчик начальных тегов 
+  function onStart($parser, $tag, $attributes){
+    if($tag != "CATALOG" and $tag != "BOOK")
+      echo "<td>";
+    if($tag == "BOOK")
+      echo "<tr>";
+  }
   // Функция обработчик закрывающих тегов 
+  function onEnd($parser, $tag){
+    if($tag != "CATALOG" and $tag != "BOOK")
+      echo "</td>";
+    if($tag == "BOOK")
+      echo "</tr>";
+  }
   // Функция обработчик текстового содержимого 
+  function onText($parser, $text){
+    echo $text;
+  }
   // Назначение обработчиков начальных и конечных тегов 
+  xml_set_element_handler($sax, "onStart", "onEnd");
   // Назначение обработчика текстового содержимого 
+  xml_set_character_data_handler($sax, "onText");
 ?>
 <html>
 
@@ -24,6 +42,7 @@
     </tr>
     <?php
       //Парсинг 
+    xml_parse($sax, file_get_contents("catalog.xml"));
     ?>
   </table>
 </body>
